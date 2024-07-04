437880db325e29227923e73302068e2cde97ad76
Rishad01 <rishadhusainrizvi786@gmail.com>
2024-07-05 03:26:22 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {Typography,Box,AppBar,Toolbar,IconButton,Button} from '@mui/material';
+
 function App() {
   return (
-    <h1>hello</h1>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
